5484f7821071888c3810efb7f99e4fe9a227c668
182Craig <107044446+182Craig@users.noreply.github.com>
2023-04-16 13:10:59 +0200

    Home Banner Title White
    Home Banner Title White

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -32,7 +32,8 @@
                 @if($heroSection == "2")
                     <div class="row slider-content align-items-center hero-section2 flex-column-reverse flex-md-row">
                         <div class="col-12 col-md-7 col-lg-6">
-                            <h1 class="text-secondary font-weight-bold">{{ $heroSectionData['title'] }}</h1>
+                            <!-- <h1 class="text-secondary font-weight-bold">{{ $heroSectionData['title'] }}</h1> -->
+                            <h1 class="home-banner-title text-secondary font-weight-bold">{{ $heroSectionData['title'] }}</h1>
                             <p class="slide-hint text-gray mt-20">{!! nl2br($heroSectionData['description']) !!}</p>
 
                             <form action="/search" method="get" class="d-inline-flex mt-30 mt-lg-30 w-100">
